2e4639bd5727681d54b58883558105114aa20f29
Lily Jeong <lily.jeong0628@gmail.com>
2022-11-05 12:41:05 +0900

    센서값 자동 모델링 완성

--- web/influx.py
+++ web/influx.py
@@ -2,32 +2,94 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 import certifi
 
-bucket = "ForPROJECT"
-org = "cb0eab07a714b4be"
-token = "uB7DjgLMtFSL1R4ID7Z509-dx5mol8xXSJOPS6mLjMM_olf137dhu3cURNKz8QOi1dK_HlbedmaoeGrxqwudww=="
-url="https://europe-west1-1.gcp.cloud2.influxdata.com/"
+def sensor():
+    bucket = "Project"
+    org = "964cfc7201acc909"
+    token = "P0eXnp8A9CvTLlxEOb3WCTiaimw20fLlvCIa4EFvqjiZQNeTf6HzqgdZ6xNJdtwv82dcjuOYRi_WzO76JXQidg=="
+    url="https://europe-west1-1.gcp.cloud2.influxdata.com"
 
-client = influxdb_client.InfluxDBClient(
-   url=url,
-   token=token,
-   org=org,
-   ssl_ca_cert=certifi.where()
-)
+    client = influxdb_client.InfluxDBClient(
+    url=url,
+    token=token,
+    org=org,
+    ssl_ca_cert=certifi.where()
+    )
 
-query_api = client.query_api()
+    query_api = client.query_api()
+    result = []
 
-query_h_avg = 'from(bucket:"ForPROJECT")\
-|> range(start: -5m)\
-|> filter(fn:(r) => r._measurement == "Sensor")\
-|> filter(fn:(r) => r.SSID == "KT_GiGA_2G_Wave2_8B8E")\
-|> filter(fn:(r) => r._field == "Humidity")\
-|> filter(fn:(r) => r.device == "ESP8266")\
-|> timedMovingAverage(every: 5m, period: 1h)\
+    # 평균값을 가져올 때 모든 센서들의 평균을 한꺼번에 가져오면 출력시 센서 순서가 계속 바뀜
+    # => 모델링 함수에 집어넣기 용이하지 않다!
+    # 따라서, 각각 센서별로 쿼리문을 따로 돌리는 방법을 택함
+
+    # 일사량
+    query_uv_avg = 'from(bucket:"Project")\
+    |> range(start: -5m, stop: now())\
+    |> filter(fn:(r) => r._measurement == "Sensor")\
+    |> filter(fn:(r) => r.SSID == "extend")\
+    |> filter(fn:(r) => r._field == "UV")\
+    |> filter(fn:(r) => r.device == "ESP8266")\
+    |> mean(column: "_value")\
     '
-humidity_avg = query_api.query(org=org, query=query_h_avg)
-results = []
-for table in humidity_avg:
-    for record in table.records:
-        results.append((record.get_field(), record.get_value()))
+    avg1 = query_api.query(org=org, query=query_uv_avg)
+    for table in avg1:
+        for record in table.records:
+            result.append(record.get_value())
 
-print(results[0])
+    # 온도
+    query_temp_avg = 'from(bucket:"Project")\
+    |> range(start: -5m, stop: now())\
+    |> filter(fn:(r) => r._measurement == "Sensor")\
+    |> filter(fn:(r) => r.SSID == "extend")\
+    |> filter(fn:(r) => r._field == "Temperature")\
+    |> filter(fn:(r) => r.device == "ESP8266")\
+    |> mean(column: "_value")\
+    '
+    avg2 = query_api.query(org=org, query=query_temp_avg)
+    for table in avg2:
+        for record in table.records:
+            result.append(record.get_value())
+
+    # 습도
+    query_h_avg = 'from(bucket:"Project")\
+    |> range(start: -5m, stop: now())\
+    |> filter(fn:(r) => r._measurement == "Sensor")\
+    |> filter(fn:(r) => r.SSID == "extend")\
+    |> filter(fn:(r) => r._field == "Humidity")\
+    |> filter(fn:(r) => r.device == "ESP8266")\
+    |> mean(column: "_value")\
+    '
+    avg3 = query_api.query(org=org, query=query_h_avg)
+    for table in avg3:
+        for record in table.records:
+            result.append(record.get_value())
+
+    # Co2농도
+    query_co2_avg = 'from(bucket:"Project")\
+    |> range(start: -5m, stop: now())\
+    |> filter(fn:(r) => r._measurement == "Sensor")\
+    |> filter(fn:(r) => r.SSID == "extend")\
+    |> filter(fn:(r) => r._field == "Co2")\
+    |> filter(fn:(r) => r.device == "ESP8266")\
+    |> mean(column: "_value")\
+    '
+    avg4 = query_api.query(org=org, query=query_co2_avg)
+    for table in avg4:
+        for record in table.records:
+            result.append(record.get_value())
+
+    #지습
+    query_soil_avg = 'from(bucket:"Project")\
+    |> range(start: -5m, stop: now())\
+    |> filter(fn:(r) => r._measurement == "Sensor")\
+    |> filter(fn:(r) => r.SSID == "extend")\
+    |> filter(fn:(r) => r._field == "Soil_moisture")\
+    |> filter(fn:(r) => r.device == "ESP8266")\
+    |> mean(column: "_value")\
+    '
+    avg5 = query_api.query(org=org, query=query_soil_avg)
+    for table in avg5:
+        for record in table.records:
+            result.append(record.get_value())
+    
+    return result
--- web/main.py
+++ web/main.py
@@ -2,6 +2,7 @@ from crypt import methods
 from re import S
 from flask import Flask, render_template, request, redirect, send_file
 from extractor.rf import extract_crop
+from influx import sensor
 import sqlite3
 import json
 
@@ -19,24 +20,30 @@ def search():
     humid = request.args.get("humidity")
     carbon = request.args.get("CO2")
     land_moist = request.args.get("land_moist")
-    if sun == '':
-        return redirect("/")
-    elif temp == '':
-        return redirect("/")
-    elif humid == '':
-        return redirect("/")
-    elif carbon == '':
-        return redirect("/")
-    elif land_moist == '':
-        return redirect("/")
-    else:
-        crop = extract_crop(sun, temp, humid, carbon, land_moist)
-        if crop == '토마토':
-            return render_template("tomato.html")
-        elif crop == '딸기':
-            return render_template("strawberry.html")
-        elif crop == '파프리카':
-            return render_template("paprika.html")
+
+    crop = extract_crop(sun, temp, humid, carbon, land_moist)
+    if crop == '토마토':
+        return render_template("tomato.html")
+    elif crop == '딸기':
+        return render_template("strawberry.html")
+    elif crop == '파프리카':
+        return render_template("paprika.html")
+
+@app.route('/sensor')
+def get_sensor():
+    sensor_data = sensor()
+    sun = sensor_data[0]
+    temp = sensor_data[1]
+    humid = sensor_data[2]
+    carbon = sensor_data[3]
+    land_moist = sensor_data[4]
+    rec = extract_crop(sun, temp, humid, carbon, land_moist)
+    if rec == '토마토':
+        return render_template("tomato.html")
+    elif rec == '딸기':
+        return render_template("strawberry.html")
+    elif rec == '파프리카':
+        return render_template("paprika.html")
 
 @app.route('/tpricing')
 def tomatoPrice():
