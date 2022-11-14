fad15d24a0398eeda640e06782ec7e7a5183b83e
Lily Jeong <lily.jeong0628@gmail.com>
2022-11-14 12:15:23 +0900

    마켓 수정본

--- web/extractor/rf.py
+++ web/extractor/rf.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.ensemble import RandomForestClassifier
+import joblib
 
 # 데이터 로드, 쉼표 제거
 veggie = pd.read_csv('./dataset.csv', thousands= ',')
@@ -13,11 +16,12 @@ data = veggie[['외부 일사량', '내부온도', '내부습도', '내부CO2', 
 target = veggie['품목명'].to_numpy()
 train_input, test_input, train_target, test_target = train_test_split(data, target, test_size=0.2, random_state=42)
 
-from sklearn.model_selection import cross_validate
-from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier(n_jobs=-1, random_state=42, n_estimators = 100, max_depth=21, min_samples_leaf=2)
 scores = cross_validate(rf, train_input, train_target, return_train_score=True, n_jobs=-1)
 rf.fit(train_input, train_target)
+
+joblib.dump(rf, 'extractor/rf_model.pkl')
+rf_model = joblib.load('extractor/rf_model.pkl')
 
 def convertString(arr):
     str_result = ""
@@ -27,6 +31,6 @@ def convertString(arr):
     return str_result
 
 def extract_crop(sun, temp, humid, carbon, land_moist):
-    prediction = rf.predict([[sun, temp, humid, carbon, land_moist]])
+    prediction = rf_model.predict([[sun, temp, humid, carbon, land_moist]])
     crop_result = convertString(prediction)
     return crop_result
